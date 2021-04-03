8a0d454a7c043b41127afdd8f3776253696dc689
Thiarhes <injesusservice@hotmail.com>
2021-04-03 12:01:57 -0300

    models created

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const uri = 
+process.env.MONGODB_URI
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((x) => {
-        console.log(`Connected to Mongo! Database name: ${x.connection[0].name}`);
+        console.log(`Connected to Mongo! Database name: ${x.connections[0].name}`);
     })
     .catch((err) => {
         console.error(`Error connecting to mongo: ${err}`);
